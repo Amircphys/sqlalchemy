c3e9fbbc8e55c58479cec3b6756f2f4ddfededc4
Amircphys <amirchik052012@gmail.com>
2025-08-28 12:56:54 +0300

    initial commit

---
+++ src/database.py
@@ -0,0 +1,27 @@
+import asyncio
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy import create_engine, text
+from config import settings
+
+engine_sync = create_engine(
+    url=settings.DATABASE_URL_psycopg,
+    echo=False,
+    pool_size=5,
+    max_overflow=10,
+)
+
+engine_async = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg,
+    echo=False,
+    pool_size=5,
+    max_overflow=10,
+)
+
+
+async def get_123():
+    async with engine_async.connect() as conn:
+        res = await conn.execute(text("SELECT VERSION()"))
+        print(f"res = {res.all()}")
+
+
+asyncio.run(get_123())
